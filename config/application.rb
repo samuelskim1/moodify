dc93b355a2e55d8128a9b68bac6b2791b4f873f9
Samuel Kim <samuelskim420@gmail.com>
2023-04-03 17:52:35 +0000

    cached public

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ require "action_view/railtie"
 require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -39,5 +40,6 @@ module Moodify
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.railties_order = [:all, :main_app]
   end
 end
