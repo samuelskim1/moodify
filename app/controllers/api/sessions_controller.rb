9840423358fb042ceebf3819a607eac4f74c6e15
Your Name <Your Email>
2023-04-02 22:05:16 -0400

    finished setting up routes and actions

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,29 @@
+class Api::SessionsController < ApplicationController
+  def show
+    if current_user
+      @user = current_user
+      render json: { user: @user }
+    else
+      render json: { user: nil }
+    end
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:credential], params[:password])
+
+    if @user
+      login!(@user)
+      render json: { user: @user }
+    else
+      render json: { errors: ['The provided credentials were invalid'] }, status: :unauthorized
+    end
+
+  end
+
+  def destroy
+    if current_user
+      logout!
+      render json: { message: 'success'}
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post 'api/test', to: 'application#test'
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [:create, :index, :show ]
+    resource :session, only: [:show, :create, :destroy]
+  end
 end
