7019a937b27b2607d968c84d235c78cf50515bf8
Samuel Kim <samuelskim420@gmail.com>
2023-04-03 21:13:20 +0000

    finished phase 3

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,12 +1,57 @@
+import React, { useState, useEffect, useReducer} from "react"
+import { useDispatch} from 'react-redux';
+import * as sessionActions from '../../store/session'
 
 
+function ProfileButton({user}) {
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
 
-function ProfileButton() {
+    const openMenu = () => {
+        if (showMenu) return;
+        setShowMenu(true);
+    }
 
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeMenu = () => {
+            setShowMenu(false);
+        };
+
+        document.addEventListener('click', closeMenu);
+
+        return () => document.removeEventListener('click', closeMenu);
+    }, [showMenu]);
+
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+    };
+
+
+    //line 30 checks first if showMenu is truthy:
+    //if it is, we run the code after the &&
+    //if it isnt, we dont run it
     return (
-        <div>
-            <i className="fa-regular fa-user"></i>
-        </div>
+        <>
+            <button onClick={openMenu}>
+                <i className="fa-regular fa-user"></i>
+            </button>
+            
+            {showMenu && (
+                <ul className="profile-dropdown">
+                    <li>{user.username}</li>
+                    <li>{user.email}</li>
+                    <li>
+                        <button onClick={logout}>Log Out</button>
+                    </li>
+                </ul>
+
+            )
+                
+            }
+        </>
     )
 
 }
