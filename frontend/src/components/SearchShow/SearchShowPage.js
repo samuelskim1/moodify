154c39a34377321fdd0a9a0bae810c3e278f8dc4
Samuel Kim <samuelskim420@gmail.com>
2023-11-04 21:46:45 +0000

    added nav, sidebar, and tracks show

--- frontend/src/components/SearchShow/SearchShowPage.js
+++ frontend/src/components/SearchShow/SearchShowPage.js
@@ -3,10 +3,16 @@ import { fetchSearchResults } from "../../store/search";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
+import SideBar from '../SideBar';
+import Navigation from '../Navigation';
+import TrackIndexItem from "../Track/TrackIndexItem";
+import TrackShow from '../Track/TrackShow';
 const Search = () => {
     // this component takes all of our search results and displays them;
     const dispatch = useDispatch();
     const history = useHistory();
+    const searchedTracks = useSelector(state => Object.values(state.search));
+    console.log(searchedTracks);
     // debugger;
     useEffect(() => {
         // this prefills our state bc when we refresh we have nothing in our state
@@ -14,13 +20,28 @@ const Search = () => {
         dispatch(fetchSearchResults(query))
     }, []);
     //make sure to make whatever we named it inside of the reducer in line 15
-    const search = useSelector((state) => state.search);
+    // const search = useSelector((state) => state.search);
+    // console.log(search);
     return (
-        <>
-            {Object.values(search).map((ele) => {
-                return <div>{ele.title}</div>
-            })}
-        </>
+        <div className="search-show-flex-container">
+            <SideBar/>
+            <div className="nav-main-container">
+                <Navigation/>
+                <div className="search-show-container">
+                    <div className="search-show-flexbox-container">
+                        {searchedTracks.map(track => (
+                            <TrackIndexItem key={track.id} track={track} />
+                        ))}
+                    </div>
+                
+                {/* {Object.values(search).map((ele) => {
+                    return <div>{ele.title}</div>
+                })} */}
+                </div>
+            </div>
+
+        </div>
+        
     );
 }
 export default Search;
