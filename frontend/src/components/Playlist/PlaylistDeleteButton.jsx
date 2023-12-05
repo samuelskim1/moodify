03a24345ccda7857483c39e738105c60503bed12
Samuel Kim <samuelskim420@gmail.com>
2023-12-05 00:26:52 +0000

    fleshed out PlaylistDeleteButton.jsx

--- frontend/src/components/Playlist/PlaylistDeleteButton.jsx
+++ frontend/src/components/Playlist/PlaylistDeleteButton.jsx
@@ -1,21 +1,32 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { setCurrentSong, setCurrentAlbum, setPrevSong, setNextSong } from '../../store/audio';
-import { fetchAlbum } from '../../store/album';
+import { deletePlaylist } from '../../store/playlist';
+import { useHistory } from 'react-router-dom';
 import './PlaylistDeleteButton.css'
 import { useEffect } from 'react';
 
 function PlaylistDeleteButton({playlist}) {
+    const dispatch = useDispatch();
+    const currentUserId = useSelector(state => state.session?.user?.id);
+    const history = useHistory();
+    // useEffect(() => {
 
-    useEffect(() => {
+    // }, [])
 
-    }, [])
 
-    const deletePlaylist = () => {
-
+    async function removePlaylist() {
+        const playlistId = playlist.id
+        console.log(playlistId);
+        
+        if (currentUserId === playlist.creatorId) {
+            await dispatch(deletePlaylist(playlistId));
+            history.push("/");
+            console.log("playlist deleted!");
+        }
+        
     }
 
     return (
-        <div className="delete-button-container" onClick={deletePlaylist}>
+        <div className="delete-button-container" onClick={removePlaylist}>
             <i class="fa-regular fa-trash-can"></i>
         </div>
     )
