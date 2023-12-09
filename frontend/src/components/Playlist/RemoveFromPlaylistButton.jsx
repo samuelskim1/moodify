42d6e0e9c6eb974e6da7ad9666feb29259e0a5a3
Samuel Kim <samuelskim420@gmail.com>
2023-12-08 21:51:26 -0500

    added more code to RemoveFromPlaylistButton

--- frontend/src/components/Playlist/RemoveFromPlaylistButton.jsx
+++ frontend/src/components/Playlist/RemoveFromPlaylistButton.jsx
@@ -1,8 +1,11 @@
+import { useDispatch } from 'react-redux';
+import { deletePlaylistTrack } from '../../store/playlist_tracks';
+import { useHistory} from 'react-router-dom';
 import './RemoveFromPlaylistButton.css'
 
 
 function RemoveFromPlaylistButton({track}) {
-
+    const dispatch = useDispatch();
     return (
         <>
             <div className="remove-track-area">
