22219963898f59735a9dbd85199d51a468d93110
Samuel Kim <samuelskim420@gmail.com>
2023-12-08 22:01:24 -0500

    fleshed out RemoveFromPlaylistButton

--- frontend/src/components/Playlist/RemoveFromPlaylistButton.jsx
+++ frontend/src/components/Playlist/RemoveFromPlaylistButton.jsx
@@ -1,22 +1,30 @@
 import { useDispatch } from 'react-redux';
 import { deletePlaylistTrack } from '../../store/playlist_tracks';
-import { useHistory} from 'react-router-dom';
+// import { useHistory} from 'react-router-dom';
 import './RemoveFromPlaylistButton.css'
 
 
 function RemoveFromPlaylistButton({track}) {
     const dispatch = useDispatch();
-    const history = useHistory();
+    // const history = useHistory();
     
 
     async function removePlaylistTrack() {
-        const playlistTrackId = track.playlistTrackId;
+        const playlistTrackId = track.id;
+        //this track.id is actually playlistTrack.id
+        //what we're passing into the params of this component is actually a playlistTrack that we get when looping through playlistTracks
+        console.log(playlistTrackId);
+
+        await dispatch(deletePlaylistTrack(playlistTrackId));
+        console.log("playlist track deleted!")
+
+
     }
 
     return (
         <>
             <div className="remove-track-area">
-                <i class="fa-solid fa-circle-minus remove-track-button"></i>
+                <i onClick={removePlaylistTrack} class="fa-solid fa-circle-minus remove-track-button"></i>
             </div>      
         </>
     )
