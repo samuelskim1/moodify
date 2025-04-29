f94c221953dd4038115955063ccca76ff7413a62
“NAME” <EMAIL@EMAIL.COM>
2025-04-29 03:57:10 -0400

    removed comments for tracks controller

--- app/controllers/api/tracks_controller.rb
+++ app/controllers/api/tracks_controller.rb
@@ -1,9 +1,5 @@
 class Api::TracksController < ApplicationController
   def index
-    #includes uses eager loading vs join's lazy loading
-    #joins we still have a n + 1 query
-    #includes avoids n + 1 query
-    #this includes the association between track since we're doing track.all, we're calling includes on every single instance of track
     @tracks = Track.includes(:artist).all
     render 'api/tracks/index'
   end
@@ -12,14 +8,6 @@ class Api::TracksController < ApplicationController
     @track = Track.find_by(id: params[:id])
     render 'api/tracks/show'
   end
-
-  # @tracks = Track.includes(:artist).all.limit(10)
-    # logic
-    # []
-    #GEt indexes of Track.All
-    # within that range, use .rand 
-    # shove the rand value into new holder array
-
 
   def splash
     random_tracks = Hash.new(0)
